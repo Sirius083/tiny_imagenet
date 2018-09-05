ec68161ce76741d458d295d87a8d22b559189994
Sirius083 <1518246621@qq.com>
2018-09-05 19:08:37 +0800

    Update input_pipe_aug.py
    calculate picture into np.array and save them to pickle file directly

--- transfer_learning/input_pipe_aug.py
+++ transfer_learning/input_pipe_aug.py
@@ -4,7 +4,6 @@ Created on Mon Aug  6 16:12:30 2018
 
 @author: Sirius
 
-
 tutorials on pytorch:
 https://cs230-stanford.github.io/pytorch-getting-started.html
 
@@ -69,13 +68,13 @@ def load_filenames_labels(mode):
         filename = 'E:\\tiny_imagenet\\tiny-imagenet-200\\val\\images\\' + split_line[0]
         label = str(label_dict[split_line[1]])
         filenames_labels.append((filename, label))
-
   return filenames_labels
 
 def parse_fn(filename, label):
     image_string = tf.read_file(filename)
     image = tf.image.decode_jpeg(image_string, channels=3)
     image = tf.image.convert_image_dtype(image, tf.float32)
+    # image = tf.image.resize_images(image,[299,299]) # resize image to 299
     return image, label
 
 # 数据不需要预处理
@@ -88,12 +87,61 @@ def input_fn(is_training):
     
     labels = tf.string_to_number(labels, tf.int32) # string to number
     labels  = tf.cast(labels, tf.uint8)
-    
+    # print('Inside input_fn, labels', labels)
+    # Note: 注意这里不能shuffle, 否则train_data和label之间不对应
     dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
     
     dataset = dataset.map(parse_fn, num_parallel_calls=NUM_PARALLEL_CALLS)
+    
+    # 应该是在这一步对输入结果打乱了
     dataset = dataset.batch(10000)
     return dataset
 
-        
+
+#==============================================================================
+# 找到label编号与class名称之间的对应关系
+# filenames_labels = load_filenames_labels('train')
+# label_dict, class_description = build_label_dicts()
+# 将数据resize成299X299
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import time
+from datetime import timedelta
+import os
+import sys
+import inception
+from input_pipe_aug import *
+from tensorflow.python.platform import gfile
+import pickle
+
+
+with tf.device(':/cpu:0'):
+  train_data = input_fn(True)
+  # val_data = input_fn(False)
+  
+train_iterator = train_data.make_one_shot_iterator()
+# val_iterator = val_data.make_one_shot_iterator()
+handle = tf.placeholder(tf.string, shape=[])
+iterator = tf.data.Iterator.from_string_handle(handle, train_iterator.output_types, train_iterator.output_shapes)
+images, labels = iterator.get_next()
+init = tf.global_variables_initializer()
+
+# 训练数据
+with tf.Session() as sess:
+    train_iterator_handle = sess.run(train_iterator.string_handle())
+    # val_iterator_handle = sess.run(val_iterator.string_handle())
+    sess.run(init)
+    stime = time.time()
+    for i in range(10):
+        train_tmp,labels_tmp = sess.run([images,labels], feed_dict = {handle: train_iterator_handle}) 
+        with open('E:\\transfer_tiny_imagenet\\data3\\train_data_' + str(i) + '.pkl', 'wb') as file:
+             pickle.dump(train_tmp, file)
+        with open('E:\\transfer_tiny_imagenet\\data3\\train_labels_' + str(i) + '.pkl', 'wb') as file:
+             pickle.dump(labels_tmp, file)
+    etime = time.time()
+print('total time', (etime-stime)/60) # 将训练集全部转化为np.array并保存 一共用时1分钟
+
+
+
 
